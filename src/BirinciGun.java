2fb8516ce2c171b588cbe0374647276026f8df1f
cevatfehmi <cevatfehmi04@gmail.com>
2020-11-17 22:29:17 +0300

    ikinci gun 2. commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,6 +1,11 @@
 
 public class BirinciGun {
 public static void main(String[] args) {
+	
 	System.out.println("11:05- Biriinci Gun");
+	System.out.println("İkinci Commit");
+	System.out.println("10:26");
+	
+	
 }
 }
