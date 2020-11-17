9399ed223bf7ccb27258fe2526f21851a6346aac
cevatfehmi <cevatfehmi04@gmail.com>
2020-11-17 23:39:31 +0300

    commit cozuldu

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,16 +3,14 @@ public class BirinciGun {
 public static void main(String[] args) {
 	
 	System.out.println("11:05- Biriinci Gun");
-<<<<<<< HEAD
 	System.out.println("İkinci Commit");
 	System.out.println("10:26");
 	System.out.println("ikinci gun");
 	System.out.println("10:38 ikinci gun");
 	 System.out.println("mesaj");
-=======
-	System.out.println("ikinci commit);
-	 System.out.println("ikinci gun);
+	System.out.println("ikinci commit");
+	 System.out.println("ikinci gun");
 	 System.out.println("11:05");
->>>>>>> refs/remotes/origin/master
+
 }
 }
