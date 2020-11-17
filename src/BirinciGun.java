80e323ba2dbf89adfadbce1fb9d99742e7c6db78
cevatfehmi <74244118+cevatfehmi@users.noreply.github.com>
2020-11-17 22:24:38 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,5 +2,7 @@
 public class BirinciGun {
 public static void main(String[] args) {
 	System.out.println("11:05- Biriinci Gun");
+	System.out.println("ikinci commit);
+	 System.out.println("ikinci gun);
 }
 }
